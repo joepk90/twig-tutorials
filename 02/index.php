d0ef5a23ed395fea2cdea8cc82764c316c4d2dd0
Joe <jparkkennaby@gmail.com>
2021-02-17 23:04:38 +0000

    06 - setup template embed example

---
+++ 02/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once '../vendor/autoload.php';;
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index-embed-example.html', array(
+    'title' => 'Twig Turoial 01',
+    'sub-title' => 'Documentation on using Twig template data',
+    'crew' => array(
+        array(
+            'name' => 'Ellen Ripley',
+            'reg-number' => '018345',
+            'role' => 'Warrent Officer',
+            'status' => 'Alive',
+        ),
+        array(
+            'name' => 'Ash',
+            'reg-number' => '018345',
+            'role' => 'Science Officer',
+            'status' => 'Dead',
+        ),
+        array(
+            'name' => 'Parker',
+            'reg-number' => '018345',
+            'role' => 'Cheif Engineer',
+            'status' => 'Alive',
+        ),
+    )
+));
