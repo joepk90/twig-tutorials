5bfc411f5d4a21fd62eef7754a7a3cf9cc173765
Joe <jparkkennaby@gmail.com>
2021-02-17 17:00:54 +0000

    02 - update data - setup index.html as an html document + present data in table

--- 01/index.php
+++ 01/index.php
@@ -5,4 +5,23 @@ require_once '../vendor/autoload.php';;
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('index.html', ['name' => 'Fabien']);
+echo $twig->render('index.html', array(
+    'title' => 'Twig Turoial 01',
+    'crew' => array(
+        array(
+            'name' => 'Ellen Ripley',
+            'role' => 'Warrent Officer',
+            'status' => 'Alive',
+        ),
+        array(
+            'name' => 'Ash',
+            'role' => 'Science Officer',
+            'status' => 'Dead',
+        ),
+        array(
+            'name' => 'Parker',
+            'role' => 'Cheif Engineer',
+            'status' => 'Alive',
+        ),
+    )
+));
