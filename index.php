31ee3e08f99611e08d27bbcef2300b3706ab1b02
Joe <jparkkennaby@gmail.com>
2021-02-17 14:54:58 +0000

    01 - setup basic twig setup. setup templates directory + index.html template

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twig Tutorial</title>
+</head>
+
+<body>
+
+    <ul>
+        <li>
+            <a href="/01">Turorial 01</a> - <a href="https://youtu.be/b_eU962en2k" class="">How To Start Theming With Twig (1/6)</a>
+        </li>
+    </ul>
+
+</body>
+
+</html>
